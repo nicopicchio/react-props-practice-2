d7cea3d013cfc58794ee2720c98454bb2fa0ac6b
Nico Picchio <nicolapicchio@gmail.com>
2022-03-01 12:42:17 +0000

    exercise completed

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,30 +1,47 @@
-import Message from './Message'
+import { useState } from 'react';
+import Message from './Message';
 
-function Chat({ messages, addMessage }) {
-  return (
-    <div className="chat">
-      <ul>
-        {messages.map( (message, index) => (
-          <Message key={index} message={message} />
-        ))}
-      </ul>
+let id = 6;
 
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          addMessage(e.target.message.value)
-          e.target.reset()
-        }}
-      >
-        <input
-          className="message-input"
-          name="message"
-          placeholder="Type a message"
-          required
-        />
-      </form>
-    </div>
-  )
+const intialMessages = [
+	{ id: 1, text: 'Hello!', user: 'Nicolas' },
+	{ id: 2, text: 'Hey!', user: 'Sergio' },
+	{ id: 3, text: 'How are you feeling today?', user: 'Nicolas' },
+	{ id: 4, text: 'Hot hot, you?', user: 'Sergio' },
+	{ id: 5, text: 'Cool cool!', user: 'Nicolas' },
+];
+
+function Chat() {
+	const [messages, setMessages] = useState(intialMessages);
+
+	const addMessage = (text) => {
+		setMessages([...messages, { id: id++, text, user: 'Nicolas' }]);
+	};
+
+	return (
+		<div className="chat">
+			<ul>
+				{messages.map((message, index) => (
+					<Message key={index} message={message} />
+				))}
+			</ul>
+
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					addMessage(e.target.message.value);
+					e.target.reset();
+				}}
+			>
+				<input
+					className="message-input"
+					name="message"
+					placeholder="Type a message"
+					required
+				/>
+			</form>
+		</div>
+	);
 }
 
-export default Chat
+export default Chat;
